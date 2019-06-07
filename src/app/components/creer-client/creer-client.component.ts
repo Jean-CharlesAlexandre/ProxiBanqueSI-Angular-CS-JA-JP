db4b17bd48b254815414aaf4fbc870e02e661327
Jérémi PIRAT <jerem.pirat@laposte.net>
2019-06-07 20:14:27 +0200

    amelioration du formulaire creation client

--- src/app/components/creer-client/creer-client.component.ts
+++ src/app/components/creer-client/creer-client.component.ts
@@ -3,6 +3,10 @@ import { ConseillerService } from 'src/app/services/conseiller.service';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Client } from 'src/app/model/client';
 import { Adresse } from 'src/app/model/adresse';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CompteCourant } from 'src/app/model/compte-courant';
+import { CompteEpargne } from 'src/app/model/compte-epargne';
+import { CarteBancaire } from 'src/app/model/carte-bancaire';
 
 @Component({
     selector: 'app-creer-client',
@@ -12,25 +16,16 @@ import { Adresse } from 'src/app/model/adresse';
 })
 export class CreerClientComponent implements OnInit {
 
+    estParticulier: boolean = true;
     clientDetails: any = {};
-    adresse: any = {};
+    createClientForm: FormGroup;
 
-    createClientForm = this.fb.group({
-        nom: ['', Validators.required],
-        prenom: ['', Validators.required],
-        email: ['', Validators.required],
-        telephone: ['', Validators.required],
-        adresse: this.fb.group({
-            numero: ['', Validators.required],
-            rue: ['', Validators.required],
-            codePostal: ['', Validators.required],
-            ville: ['', Validators.required]
-        })
-    });
-
-    constructor(private conseillerService: ConseillerService, private fb: FormBuilder) {
+    constructor(private conseillerService: ConseillerService, private fb: FormBuilder, private router: Router) {
         this.clientDetails = new Client();
         this.clientDetails.adresse = new Adresse();
+        this.clientDetails.compteCourant = new CompteCourant();
+        this.clientDetails.compteEpargne = new CompteEpargne();
+        this.clientDetails.carteBancaire = new CarteBancaire();
     }
 
     onSubmit() {
@@ -39,20 +34,46 @@ export class CreerClientComponent implements OnInit {
         this.clientDetails.email = this.createClientForm.value.email;
         this.clientDetails.telephone = this.createClientForm.value.telephone;
 
-        this.clientDetails.adresse.numero = this.createClientForm.value.numero;
-        this.clientDetails.adresse.rue = this.createClientForm.value.rue;
-        this.clientDetails.adresse.codePostal = this.createClientForm.value.codePostal;
-        this.clientDetails.adresse.ville = this.createClientForm.value.ville;
+        this.clientDetails.adresse.numero = this.createClientForm.value.adresse.numero;
+        this.clientDetails.adresse.rue = this.createClientForm.value.adresse.rue;
+        this.clientDetails.adresse.codePostal = this.createClientForm.value.adresse.codePostal;
+        this.clientDetails.adresse.ville = this.createClientForm.value.adresse.ville;
 
         this.createClient();
     }
 
     createClient() {
-        this.conseillerService.createClient(this.clientDetails).subscribe((data: {}) => console.log(data));
+        this.conseillerService.createClient(this.clientDetails).subscribe((data: {}) => this.router.navigate(['/liste-clients']));
     }
 
+    onParticulier() {
+        this.estParticulier = true;
+    }
+
+    onEntreprise() {
+        this.estParticulier = false;
+    }
 
     ngOnInit() {
+        this.createClientForm = this.fb.group({
+            raisonSociale: ['', Validators.required],
+            nom: ['', Validators.required],
+            prenom: ['', Validators.required],
+            email: ['', Validators.required],
+            telephone: ['', Validators.required],
+            adresse: this.fb.group({
+                numero: ['', Validators.required],
+                rue: ['', Validators.required],
+                codePostal: ['', Validators.required],
+                ville: ['', Validators.required]
+            }),
+            compteCourant: ['', Validators.required],
+            soldeCompteCourant: ['', Validators.required],
+            compteEpargne: ['', Validators.required],
+            soldeCompteEpargne: ['', Validators.required],
+            carteBancaire: ['', Validators.required],
+            typeCarteBancaire: ['', Validators.required]
+        });
     }
 
 }
